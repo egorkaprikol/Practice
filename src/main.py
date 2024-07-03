76535671690ed90b3b577039b84b3a732e0a5c3f
Egor <zherelev92@mail.ru>
2024-07-03 12:26:57 +0300

    created patients entity

--- src/main.py
+++ src/main.py
@@ -1,13 +1,52 @@
-from fastapi import FastAPI
+from typing import Annotated
+from fastapi import FastAPI, Depends
+from pydantic import BaseModel
+from src.models import models
+from src.database.config import engine, SessionLocal
+from sqlalchemy.orm import Session
+
+
+models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+db_dependency = Annotated[Session, Depends(get_db)]
+
+
+class PatientBase(BaseModel):
+    id: int
+    name: str
+    surname: str
+    fathername: str
+    gender: str
+    age: int
+    sector: int
+    number: int
+    address: str
+
+
+class GenderBase(BaseModel):
+    value: str
+
+
+class SectorBase(BaseModel):
+    number: int
+    address: str
+
+@app.post("/pacients/")
+async def create_pacient(pacient: PatientBase, db: db_dependency):
+    db_pacient = models.Pacient()
+    db.add(db_pacient)
+    db.commit()
+    db.refresh(db_pacient)
+    return db_pacient
+
